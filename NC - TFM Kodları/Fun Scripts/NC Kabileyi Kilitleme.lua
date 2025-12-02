046d77e1a42bf5e90e06888325d2c008e0a88034
Oğuz Taşdemir <93481181+oguztasdemir@users.noreply.github.com>
2025-12-02 21:35:11 +0300

    Update NC Kabileyi Kilitleme.lua

--- NC - TFM Kodları/Fun Scripts/NC Kabileyi Kilitleme.lua
+++ NC - TFM Kodları/Fun Scripts/NC Kabileyi Kilitleme.lua
@@ -1,11 +1,10 @@
---Created by Zagear and translated from AR to EN by Hmiida
 admin = "Devilstrkpro#5475" -- Admin's name
 password = "NCLA" -- Password
 players={} -- Don't write anything here
 system.disableChatCommandDisplay("password")
  
 function pw(name)
-print("<font color='#ffffff'> Kabile evi þifrelendirmesi aktifleþtirildi.")
+print("<font color='#ffffff'> Kabile evi şifrelendirmesi aktifleştirildi.")
 for name,player in pairs(tfm.get.room.playerList) do
 if name == admin then
 for poups, k in pairs({"1","2","3","4","5","6"}) do
@@ -15,11 +14,11 @@ else
 ui.addTextArea(1, "", name, -1499, -1500, 3000, 3000, 0x000001, 0x000001, 1, true)
 ui.addTextArea(2, "", name, 5, 28, 789, 367, 0x544731, 0xffffff, 1, true)
 ui.addTextArea(3, "", name, 8, 31, 783, 361, 0x324650, 0x000000, 1, true)
-ui.addTextArea(4, "\n<p align='center'><font color='#babd2f' size='25' face='Comic Sans MS'><b>Tüm oyun içi bilgilendirme, bu bir test mesajıdır. Devam etmek için şifrenizi girebilirsiniz. </b>\n\n<p align='center'><font color='#babd2f' size='25' face='Comic Sans MS'>Lütfen aktivasyon þifresini girin.</b>\n\n<p align='center'><font color='#babd2f' size='25' face='Comic Sans MS'>Ýpucu: Aktivasyon þifresi, Transformice hesabýnýzýn þifresidir.", name, 11, 34, 777, 355, 0x523d25, 0xc4621c, 1, true)
+ui.addTextArea(4, "\n<p align='center'><font color='#babd2f' size='25' face='Comic Sans MS'><b>Tüm oyun içi bilgilendirme, bu bir test mesajıdır. Devam etmek için şifrenizi girebilirsiniz. </b>\n\n<p align='center'><font color='#babd2f' size='25' face='Comic Sans MS'>Lütfen aktivasyon şifresini girin.</b>\n\n<p align='center'><font color='#babd2f' size='25' face='Comic Sans MS'>İpucu: Hesabınıza giriş yapmalısınız!", name, 11, 34, 777, 355, 0x523d25, 0xc4621c, 1, true)
 ui.addTextArea(5, "<p align='center'><font color='#babd2f' size='25' face='Comic Sans MS'><b>"..players[name].txt.."", name, 431, 302, 344, 71, 0x324650, 0x000000, 0, true)
 ui.addTextArea(6, "<font color='#ffffff'>", name, 679, 359, 106, 27, 0x523d25, 0x000000, 1, true)
 ui.updateTextArea(6,"<font color='#009D9D'> "..players[name].try.." <font color='#ffffff'> : Kalan hak remaining",name)
-ui.addPopup(0, 2, "<p align='center'> Þifre giriþ yeri:", name, 296, 305, 217, true)
+ui.addPopup(0, 2, "<p align='center'> Şifre girme ekranı:", name, 296, 305, 217, true)
 end
 end
 end
@@ -96,3 +95,4 @@ end
 table.foreach(tfm.get.room.playerList, eventNewPlayer)
 
 pw(name)
+
